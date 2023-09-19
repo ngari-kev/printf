685af342fbcb3eb63e65b4a14350110da9a32b7b
Nyamisachela <nyamisachelagat@gmail.com>
2023-09-19 07:48:50 -0700

    New printf file including of d and i conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,48 +1,79 @@
 #include "main.h"
+#include <stdarg.h>  /* Include the necessary header for va_list and related functions */
+
 /**
-  *_printf - produces output according to a format.
-  *@format: array of specifiers
-  *Return: Number of characters printed to stdout
-  */
+ * _printf - produces output according to a format.
+ * @format: array of specifiers
+ * Return: Number of characters printed to stdout
+ */
 int _printf(const char *format, ...)
 {
-	int xters = 0, i = 0;/*xters is the number of characters*/
-	char c, *str;
-	va_list ap;
+        int xters = 0;  /* Number of characters printed */
+        va_list ap;     /* Initialize the va_list for variable arguments */
+        int len = 0; /*move var declaration to the top*/
+        if (format == NULL)
+                return (-1);
 
-	if (format == NULL)
-		return (-1);
-	va_start(ap, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			xters++;
-		}
-		else if (*format == '%')
-		{
-			format++;/*if *format == %, we move to the next item*/
-			if (*format == '%')/*the case of double %*/
-			{
-				write(1, format, 1);
-				xters++;
-			}
-			else if (*format == 'c')
-			{
-				c = va_arg(ap, int);
-				write(1, &c, 1);
-				xters++;
-			}
-			else if (*format == 's')
-			{
-				str = va_arg(ap, char*);
-				i = strlen(str);
-				write(1, str, i);
-				xters += i;
-			}
-		}
-		format++;
-	}
-	return (xters);
+        va_start(ap, format);  /* Initialize the va_list with variable arguments */
+
+        while (*format)
+        {
+                if (*format != '%')
+        {
+                write(1, format, 1);
+                xters++;
+        }
+                else if (*format == '%')
+                {
+                        format++;
+                        if (*format == '%')
+                        {
+                                write(1, format, 1);
+                                xters++;
+                        }
+                        else if (*format == 'c')
+                        {
+                                char c = va_arg(ap, int);  /* Correctly retrieve char */
+                                write(1, &c, 1);
+                                xters++;
+                        }
+                        else if (*format == 's')
+                        {
+                                char *str = va_arg(ap, char *);
+                                if (str == NULL)
+                                        return (-1);
+                                /* int len = 0;*/
+                                while (str[len] != '\0')
+                                        len++;
+                                write(1, str, len);
+                                xters += len;
+                        }
+                        else if (*format == 'd' || *format == 'i')
+                        {
+                                int num = va_arg(ap, int);  /* Correctly retrieve int */
+                                char buffer[20];  /* Create a buffer to hold the number as a string */
+                                int len = 0;
+                                if (num < 0)
+                                {
+                                        write(1, "-", 1);  /* Print a minus sign for negative numbers */
+                                        num = -num;  /* Make num positive for conversion */
+                                        len++;
+                                }
+                                do
+                                {
+                                        buffer[len++] = num % 10 + '0';  /* Convert digits to characters */
+                                        num /= 10;
+                                }
+                                while (num > 0);
+                                /* Reverse the buffer to get the correct number */
+                                for (len = len - 1; len >= 0; len--)
+                                        write(1, &buffer[len], 1);
+                                xters += len;
+                        }
+                }
+                format++;
+        }
+        va_end(ap);  /* Clean up the va_list */
+        return (xters);
 }
+
